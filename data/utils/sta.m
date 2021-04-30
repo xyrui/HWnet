f6568b49d1807d107deb2e47990127e54f159bd5
Xiangyu Rui <xyrui@outlook.com>
2021-04-30 09:51:39 +0800

    Add files via upload

---
+++ data/utils/sta.m
@@ -0,0 +1,10 @@
+function im = sta(im)
+if length(size(im)) == 2
+    ma = max(im(:));
+    mi = min(im(:));
+    im = (im - mi)/(ma - mi);
+else
+    ma = max(max(im,[],1),[],2);
+    mi = min(min(im,[],1),[],2);
+    im = (im - mi)./(ma - mi);
+end
